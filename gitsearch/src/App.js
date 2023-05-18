664f712c7483fe1b823827d01e9c6016bfeebd81
Kelsey Lucero <kelsey.cribari@gmail.com>
2023-05-17 19:27:41 -0600

    Getting the basic users API call working with test data

--- gitsearch/src/App.js
+++ gitsearch/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react"; 
+import { Octokit } from "https://cdn.skypack.dev/octokit";
+import {
+  Box,
+  Data,
+  Header,
+  List,
+  Grommet,
+  Page } from 'grommet'; 
 
 function App() {
+
+  const [users, setUsers] = useState(''); 
+
+  const octokit = new Octokit({
+    auth: process.env.REACT_APP_GH
+  })
+
+   
+  useEffect(() => {
+    async function getUsers() {
+      await octokit.request('GET /users/{username}', {
+        username: 'kelseycribari',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      }).then((response) => {
+        console.log(response); 
+        setUsers(response.data);
+      });
+    }
+    getUsers();
+  },[]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grommet full={true}>
+        <Page>
+          <Box>
+            <Header fill="horizontal"
+              pad={{ horizontal: 'medium', vertical: 'small' }}
+              background="background-front">
+            </Header>
+          </Box>
+        </Page>
+      </Grommet>
     </div>
   );
 }
