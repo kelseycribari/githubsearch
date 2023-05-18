c81ff9ae626164d3f92c07166c4c796a9316cc85
Kelsey Lucero <kelsey.cribari@gmail.com>
2023-05-17 20:28:02 -0600

    Moving users API call to User.js component

--- gitsearch/src/App.js
+++ gitsearch/src/App.js
@@ -1,39 +1,13 @@
 import './App.css';
-import React, {useState, useEffect} from "react"; 
-import { Octokit } from "https://cdn.skypack.dev/octokit";
+import React, { useState, useEffect } from "react"; 
+import User from './components/User';
 import {
   Box,
-  Data,
   Header,
-  List,
   Grommet,
   Page } from 'grommet'; 
 
 function App() {
-
-  const [users, setUsers] = useState(''); 
-
-  const octokit = new Octokit({
-    auth: process.env.REACT_APP_GH
-  })
-
-   
-  useEffect(() => {
-    async function getUsers() {
-      await octokit.request('GET /users/{username}', {
-        username: 'kelseycribari',
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      }).then((response) => {
-        console.log(response); 
-        setUsers(response.data);
-      });
-    }
-    getUsers();
-  },[]);
-
-
 
   return (
     <div className="App">
@@ -45,6 +19,7 @@ function App() {
               background="background-front">
             </Header>
           </Box>
+          <User/>
         </Page>
       </Grommet>
     </div>
--- gitsearch/src/components/User.js
+++ gitsearch/src/components/User.js
@@ -1,6 +1,40 @@
-const User = () => {
-    
+import React, { useState, useEffect } from "react"; 
+import { Octokit } from "https://cdn.skypack.dev/octokit";
+import {
+  Box
+} from 'grommet';
 
+const User = (props) => {
+
+    //grab the user's name from the path by removing all leading and trailing slashes
+    const [pathName] = useState(window.location.pathname.replace(/\//g,''));  
+    const [users, setUsers] = useState(''); 
+
+    //initialize the Octokit variable and set the authentication to the token located in .env.local
+    const octokit = new Octokit({
+        auth: process.env.REACT_APP_GH
+      })
+
+    useEffect(() => {
+        async function getUsers() {
+          //makes the get request to grab the user based on the username given in the path
+          await octokit.request('GET /users/{username}', {
+            username:  pathName,
+            headers: {
+              'X-GitHub-Api-Version': '2022-11-28'
+            }
+          }).then((response) => {
+            console.log(response); 
+            console.log(pathName);
+            setUsers(response.data);
+          });
+        }
+        getUsers();
+      },[]);
+
+      return (
+        <Box></Box>
+      );
 }
 
 export default User;
